f86860ca88039d7cb02b181b513ac8d256d8fc97
YashSharma412 <sharmayashsatiish@gmail.com>
2024-01-19 20:15:41 +0530

    prettier

--- src/components/Relatives.jsx
+++ src/components/Relatives.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function Relatives() {
-    const relatives = [
-        "Manju", "Yash", 
-        "Raghu", "Rajan",
-        "Siddharth", "Rohan"
-    ]
+  const relatives = ["Manju", "Yash", "Raghu", "Rajan", "Siddharth", "Rohan"];
   return (
     <div>
-        <h1>Relatives List</h1>
-        <ol id='relativeList' key="relativeList">
-            {
-                relatives.map((relative, index) => {
-                    return <li id={`relativeListItem${index+1}`} key={`relativeListItem${index+1}`}>{relative}</li>
-                })
-            }
-        </ol>
+      <h1>Relatives List</h1>
+      <ol id="relativeList" key="relativeList">
+        {
+            relatives.map((relative, index) => {
+                return (
+                  <li
+                    id={`relativeListItem${index + 1}`}
+                    key={`relativeListItem${index + 1}`}
+                  >
+                    {relative}
+                  </li>
+                );
+              })
+        }
+      </ol>
     </div>
-  )
+  );
 }
 
-export default Relatives
+export default Relatives;
